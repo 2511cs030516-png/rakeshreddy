1f3ee50e720dad3c5a6eeea1a5060e4617bc5541
2511cs030516-png <2511cs030516@mallareddyuniversity.ac.in>
2026-07-16 09:21:34 +0530

    Add files via upload

--- EcommerceString.java
+++ EcommerceString.java
@@ -2,20 +2,22 @@ public class EcommerceString {
     public static void main(String[] args) {
 
         // Product Details
-        String productName = "Laptop";
-        String category = "Electronics";
-        String brand = "HP";
-        String customerName = "Srinivas";
-        String paymentMethod = "UPI";
-        String orderStatus = "Delivered";
+        String customerName = "Rakesh";
+        String productName = "Samsung Galaxy S24";
+        String category = "Mobile";
+        String brand = "Samsung";
+        String paymentMethod = "Credit Card";
+        String deliveryAddress = "Hyderabad";
+        String orderStatus = "Shipped";
 
         // Display Details
-        System.out.println("===== E-Commerce Order =====");
-        System.out.println("Customer Name : " + customerName);
-        System.out.println("Product Name  : " + productName);
-        System.out.println("Category      : " + category);
-        System.out.println("Brand         : " + brand);
-        System.out.println("Payment Method: " + paymentMethod);
-        System.out.println("Order Status  : " + orderStatus);
+        System.out.println("===== E-Commerce Order Details =====");
+        System.out.println("Customer Name   : " + customerName);
+        System.out.println("Product Name    : " + productName);
+        System.out.println("Category        : " + category);
+        System.out.println("Brand           : " + brand);
+        System.out.println("Payment Method  : " + paymentMethod);
+        System.out.println("Delivery Address: " + deliveryAddress);
+        System.out.println("Order Status    : " + orderStatus);
     }
 }
